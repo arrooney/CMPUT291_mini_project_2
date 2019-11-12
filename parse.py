0cb4749ea21376f1a516e16543b11b8061f5be90
arrooney <arrooney@ualberta.ca>
2019-11-11 23:24:22 -0700

    parse XML one  line at a time from standard input

---
+++ parse.py
@@ -0,0 +1,67 @@
+import sys, re
+
+
+class parseMail(object):
+
+	def __match__(self, pattern):
+		# check if the current token matches the pattern, increment current if so
+		if not self.thisToken:
+			self.thisToken = sys.stdin.readline()
+		if re.search(pattern, self.thisToken):
+			self.thisToken = sys.stdin.readline()
+			return True
+		return False
+
+	def __tokenPeek__(self):
+		return self.thisToken
+
+	def __consume__(self):
+		tmp = self.thisToken
+		self.thisToken = sys.stdin.readline()
+		return tmp
+
+	def __init__(self):
+		self.thisEmail = {}
+		self.current = 0
+		self.thisToken = None
+
+	def execute(self):
+		self.__mail__()
+
+	def __mail__(self):
+		while(self.__match__("^\s*(<mail>)|(<emails type=[\w\"\']*>)\s*$")):
+			self.thisEmail.clear()
+			self.__emailInfo__()
+
+	def __emailInfo__(self):
+		while(not self.__match__("^\s*</mail>\s*$")):
+			if re.search("^\s*<([\w]+)>([\s\w/!@#$%^&*\.:\-,\?\(\)\"\'\+=\\/\[\]\{\}]+)</[\w]+>\s*$", self.__tokenPeek__()):
+				# this line contains 
+				tagVal = re.findall("^\s*<([\w]+)>([\s\w/!@#$%^&*\.:\-,\?\(\)\"\'\+=\\/\[\]\{\}]+)</[\w]+>\s*$", self.__consume__())
+				self.thisEmail[tagVal[0][0]] = tagVal[0][1]
+			if re.search("^\s*<([a-zA-z]+)>\s*$", self.__tokenPeek__()):
+				# open tag with contents as next token
+				tagname = re.findall("^\s*<([a-zA-z]+)>\s*$", self.__consume__())
+				self.thisEmail[tagname[0]] = self.__consume__()
+			elif re.search("^\s*<([a-zA-z]+)/>\s*$", self.__tokenPeek__()):
+				# empty tag
+				# give email an empty tagname
+				tagname = re.findall("^\s*<([a-zA-z]+)/>\s*$", self.__consume__())
+				self.thisEmail[tagname[0]] = ""
+			elif re.search("^\s*</([a-zA-z]+)>\s*$", self.__tokenPeek__()):
+				# end of valid open tag
+				tag = re.findall("^\s*</([a-zA-z]+)>\s*$", self.__consume__())
+				if tag[0] == "mail":
+					break
+		print(self.thisEmail)
+
+
+
+
+def main():
+	parser = parseMail()
+	parser.execute()
+
+
+if __name__ == "__main__":
+	main()
