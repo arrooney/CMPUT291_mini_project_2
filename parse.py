98df3be1dba89214b2d0475d5827aa5ec2bef2fa
arrooney <arrooney@ualberta.ca>
2019-11-12 02:31:01 -0700

    debug parser, and write to term.txt.

--- parse.py
+++ parse.py
@@ -1,13 +1,14 @@
 import sys, re
 
 
-class parseMail(object):
+class ParseMail(object):
 
 	# CONSTRUCTOR:
 	def __init__(self):
 		self.thisEmail = {}
 		self.current = 0
 		self.thisToken = None
+		self.term = None
 
 	## PUBLIC METHODS: ##
 
@@ -18,33 +19,66 @@ class parseMail(object):
 	## PRIVATE METHODS: ##
 
 	def __mail__(self):
+		self.__openFiles__()
 		while(self.__match__("^\s*(<mail>)|(<emails type=[\w\"\']*>)\s*$")):
 			self.thisEmail.clear()
 			self.__emailInfo__()
+		self.__closeFiles__()
 
 	def __emailInfo__(self):
 		while(not self.__match__("^\s*</mail>\s*$")):
-			if re.search("^\s*<([\w]+)>([\s\w/!@#$%^&*\.:\-,\?\(\)\"\'\+=\\/\[\]\{\}]+)</[\w]+>\s*$", self.__tokenPeek__()):
+			if re.search("^\s*<([\w]+)>([\s\w/!@#$%^&*\.:\-,\?\(\)\"\'\+=\\/\[\]\{\}]+)?</[\w]+>\s*$", self.__tokenPeek__()):
+				# <tag>data</tag>
 				# this line contains 
-				tagVal = re.findall("^\s*<([\w]+)>([\s\w/!@#$%^&*\.:\-,\?\(\)\"\'\+=\\/\[\]\{\}]+)</[\w]+>\s*$", self.__consume__())
+				tagVal = re.findall("^\s*<([\w]+)>([\s\w/!@#$%^&*\.:\-,\?\(\)\"\'\+=\\/\[\]\{\}]+)?</[\w]+>\s*$", self.__consume__())
 				self.thisEmail[tagVal[0][0]] = tagVal[0][1]
+
 			if re.search("^\s*<([a-zA-z]+)>\s*$", self.__tokenPeek__()):
+				# <tag>
+				# next token is data
 				# open tag with contents as next token
 				tagname = re.findall("^\s*<([a-zA-z]+)>\s*$", self.__consume__())
-				self.thisEmail[tagname[0]] = self.__consume__()
-			elif re.search("^\s*<([a-zA-z]+)/>\s*$", self.__tokenPeek__()):
+				if (tagname[0] != "mail"):
+					self.thisEmail[tagname[0]] = self.__consume__()
+
+			if re.search("^\s*<([a-zA-z]+)/>\s*$", self.__tokenPeek__()):
+				# <emptyTag/>
 				# empty tag
 				# give email an empty tagname
 				tagname = re.findall("^\s*<([a-zA-z]+)/>\s*$", self.__consume__())
 				self.thisEmail[tagname[0]] = ""
-			elif re.search("^\s*</([a-zA-z]+)>\s*$", self.__tokenPeek__()):
+
+			if re.search("^\s*</([a-zA-z]+)>\s*$", self.__tokenPeek__()):
+				# </endTag>
 				# end of valid open tag
 				tag = re.findall("^\s*</([a-zA-z]+)>\s*$", self.__consume__())
 				if tag[0] == "mail":
 					break
-		print(self.thisEmail)
+		self.__createTerm__()
+
+	def __createTerm__(self):
+		if self.thisEmail != {}:
+			# sanitize output and write to term.txt
+			id = self.thisEmail["row"]
+			# subject line
+			for term in self.thisEmail["subj"].split(" "):
+				term = re.sub("[^a-zA-Z0-9\-_]*|&#[\d]+", "", term)
+				if len(term) > 2:
+					self.term.write("s-" + term.lower() + ":" + id + "\n")
+			# body
+			for term in self.thisEmail["body"].split(" "):
+				term = re.sub("[^a-zA-Z\-_]*|&#[\d]+", "", term)
+				if len(term) > 2:
+					self.term.write("b-" + term.lower() + ":" + id + "\n")
+
 
 	## HELPER METHODS: ##
+
+	def __openFiles__(self):
+		self.term = open("term.txt", mode = 'w', encoding = 'utf-8')
+
+	def __closeFiles__(self):
+		self.term.close()
 
 	def __match__(self, pattern):
 		# check if the current token matches the pattern, increment current if so
@@ -65,7 +99,7 @@ class parseMail(object):
 
 
 def main():
-	parser = parseMail()
+	parser = ParseMail()
 	parser.execute()
 
 
