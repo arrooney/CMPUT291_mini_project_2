249b68edf5f9add0bbec3514556bb943e07fd9c8
arrooney <arrooney@ualberta.ca>
2019-11-20 20:57:12 -0700

    create DB instances on the fly to not call open() twice

--- idxParse.py
+++ idxParse.py
@@ -27,11 +27,19 @@ class QueryParse(object):
             self.tokens = tokens # list of tokens from the user
             self.current = 0
             self.mode = "brief" # brief by default
-            self.mydb = db.DB()
-            self.idResult = set([]) # instantiate the database object
+            self.idResult = set()
             # mydb.open("idx/da.idx", None, db.DB_BTREE, db.DB_RDONLY)
             # cursor = mydb.cursor()
             # mydb.close()
+
+        def __cursor__(self, path, treeType):
+            tmp = db.DB()
+            data = db.DB_BTREE if treeType == "btree" else db.DB_HASH
+            tmp.open(path, None, data, db.DB_RDONLY)
+            return [tmp, tmp.cursor()]
+
+        def __closeConn__(self, db):
+            db.close()
 
         def __rewind__(self):
             self.current -= 1
@@ -49,7 +57,9 @@ class QueryParse(object):
             return self.__command__()
 
         def __command__(self):
-            if (self.__match__("^output=[\w]$")):
+            print(self.__currentToken__())
+
+            if (self.__match__("^output=[\w]+$")):
                 self.__rewind__()
                 return self.__modeChange__()
             else:
@@ -73,55 +83,54 @@ class QueryParse(object):
             if self.__match__("^date$"):
                 self.__dateQuery__()
 
-            elif self.__match__("^cc$ | ^bcc$ | ^from$ | ^to$"):
+            elif self.__match__("^cc$|^bcc$|^from$|^to$"):
                 self.__rewind__()
                 self.__emailQuery__()
 
             else:
                 self.__rewind__()
                 self.__termQuery__()
-
             return True
 
         def __dateQuery__(self):
-            mydb.open("idx/da.idx", None, db.DB_BTREE, db.DB_RDONLY)
-            cursor = mydb.cursor()
+            [db, cursor] = self.__cursor__("idx/da.idx", "btree")
 
             if self.__match__("^:$"):
+                print("here")
                 result = cursor.set(self.__currentToken__().encode("utf-8"))
-                idResult.add(result)
-
+                self.idResult.add(result)
 
             elif self.__match__("^>=$"):
                 result = cursor.set_range(self.__currentToken__().encode("utf-8"))
                 while (result != None):
                     result = cursor.next()
-                    idResult.add(result)
+                    self.idResult.add(result)
 
             elif self.__match__("^<=$"):
                 result = cursor.set_range(self.__currentToken__().encode("utf-8"))
                 while (result != None):
                     result = cursor.prev()
-                    idResult.add(result)
+                    self.idResult.add(result)
 
             elif self.__match__("^>$"):
                 result = cursor.set_range(self.__currentToken__().encode("utf-8"))
                 while (result != None):
                     if self.__currentToken__() != result[0].decode("utf-8"): #excludes the current token since this is exclusive
                         result = cursor.next()
-                        idResult.add(result)
+                        self.idResult.add(result)
                         
             elif self.__match__("^<$"):
                 result = cursor.set_range(self.__currentToken__().encode("utf-8"))
                 while (result != None):
                     if self.__currentToken__() != result[0].decode("utf-8"): #excludes the current token since this is exclusive
                         result = cursor.prev()
-                        idResult.add(result)
-            mydb.close()
+                        self.idResult.add(result)
+
+            self.__closeConn__(db)
             return
+        
         def __emailQuery__(self):
-            mydb.open("idx/em.idx", None, db.DB_BTREE, db.DB_RDONLY)
-            cursor = mydb.cursor()
+            [db, cursor] = self.__cursor__("idx/em.idx", "btree")
 
             emailString = ""
             emailString += self.__currentToken__()
@@ -130,25 +139,26 @@ class QueryParse(object):
 
             #iterate through every single entry in the query
                 #Check and return all emails that have a key that matches emailString
-
+            self.__closeConn__(db)
             return
+       
         def __termQuery__(self):
-            mydb.open("idx/recs.idx", None, db.DB_BTREE, db.DB_RDONLY)
-            cursor = mydb.cursor()
+            [db, cursor] = self.__cursor__("idx/re.idx", "hash")
 
-            if self.__match__("^subj$")
+            if self.__match__("^subj$"):
                 self.__current__+=1 #do this because the term after subj is the delimiter,
 
-            elif self.__match__("^body$")
+            elif self.__match__("^body$"):
                 self.__current__+=1 #do this because the term after body is the delimiter
 
-            else #this will be the condition where term prefix is 0
+            else: return #this will be the condition where term prefix is 0
                 #do something
+            self.__closeConn__(db)
             return
 
 
 def lexer(query):
-    tmp = list(filter(None, re.split(r"([a-zA-Z0-9_\-]+)|(^[:<>]{1}$) | (^output=[\w]+$) | (^[<>=]{2}$)", query)))
+    tmp = list(filter(None, re.split(r" |([a-zA-Z0-9_\-]+)|([:<>]{1})|(^output=[\w]+$)|(^[<>=]{2}$)", query)))
     tokens = []
     [tokens.append(x) for x in tmp if not str(x).strip() == ""]
     return tokens
@@ -156,11 +166,11 @@ def lexer(query):
 
 
 def main():
-    query = input("enter query")
+    query = input("enter query:\n")
     tokens = lexer(query)
     print(tokens)
-    # parser = QueryParse(tokens)
-    # print(parser.execute())
+    parser = QueryParse(tokens)
+    print(parser.execute())
 
 
 if __name__ == "__main__":
