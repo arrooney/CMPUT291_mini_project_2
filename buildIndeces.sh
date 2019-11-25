63d3a3c35cf5a17d49f5d6fc8054ef5ce75d1075
atonyma7 <43189616+atonyma7@users.noreply.github.com>
2019-11-25 13:02:55 -0700

    actually restored buildIndeces

--- buildIndeces.sh
+++ buildIndeces.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 ###
 #	CMPUT 291 MiniProject 2
 ###
@@ -7,29 +6,29 @@
 DIR="./idx/"
 
 if [ -f ${DIR} ]; then
+if [ -e ${dir} ]
+then
 	rm -r ${DIR}
 fi
 
 if ! [ -f ${DIR} ]; then
 	mkdir ${DIR}
 fi
+mkdir ${DIR}
 
 # sort the files, use db_load to create the index
 if [ -f "terms.txt" ]; then
 	sort terms.txt -u -o terms.txt
 	cat terms.txt | ./break.pl | db_load -c duplicates=1 -T -t btree ${DIR}te.idx
 fi
-
 if [ -f "recs.txt" ]; then
 	sort recs.txt -u -n -o recs.txt
 	cat recs.txt | ./break.pl | db_load -c duplicates=1 -T -t hash ${DIR}re.idx
 fi
-
 if [ -f "emails.txt" ]; then
 	sort emails.txt -u -o emails.txt
 	cat emails.txt | ./break.pl | db_load -c duplicates=1 -T -t btree ${DIR}em.idx
 fi
-
 if [ -f "dates.txt" ]; then
 	sort dates.txt -u -o dates.txt
 	cat dates.txt | ./break.pl | db_load -c duplicates=1 -T -t btree ${DIR}da.idx
